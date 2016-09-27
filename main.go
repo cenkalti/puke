d95b8e8b2827285bbff1a3408e208a370c3569fc
Cenk Alti <cenkalti@gmail.com>
2016-09-27 15:43:25 +0300

    init

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+var (
+	addr       = flag.String("addr", ":8080", "listen address")
+	key        = flag.String("key", "", "secret key")
+	bufferSize = flag.Int("buffer-size", 64*1024, "buffer size in bytes")
+)
+
+func main() {
+	flag.Parse()
+
+	http.HandleFunc("/", puke)
+	http.ListenAndServe(*addr, nil)
+}
+
+func puke(w http.ResponseWriter, r *http.Request) {
+	if r.FormValue("key") != *key {
+		http.Error(w, "invalid key", 401)
+		return
+	}
+	bufferSizeStr := r.FormValue("bufferSize")
+	if bufferSizeStr == "" {
+		bufferSizeStr = "256000"
+	}
+	bufferSize, err := strconv.Atoi(bufferSizeStr)
+	if err != nil {
+		http.Error(w, "invalid bufferSize param", 400)
+		return
+	}
+	durationStr := r.FormValue("duration")
+	if durationStr == "" {
+		durationStr = "20s"
+	}
+	duration, err := time.ParseDuration(durationStr)
+	if err != nil {
+		http.Error(w, "invalid duration param", 400)
+		return
+	}
+	var buffer = make([]byte, bufferSize)
+	var startTime = time.Now()
+	for {
+		_, err := w.Write(buffer)
+		if err != nil {
+			return
+		}
+		if time.Now().Sub(startTime) > duration {
+			return
+		}
+	}
+}
