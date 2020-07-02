da200139ec4a859045ef79352ae389113a1eb561
Cenk Alti <cenkalti@gmail.com>
2020-07-02 13:02:46 +0300

    use same buffer for all requests

--- main.go
+++ main.go
@@ -2,54 +2,47 @@ package main
 
 import (
 	"flag"
+	"log"
 	"net/http"
-	"strconv"
 	"time"
 )
 
 var (
-	addr = flag.String("addr", ":8080", "listen address")
+	addr            = flag.String("addr", ":8080", "listen address")
+	bufferSize      = flag.Int("buffer-size-kb", 256, "write buffer size in KB")
+	defaultDuration = flag.Duration("default-duration", 20*time.Second, "response is generated for this duration")
 )
 
-const (
-	defaultBufferSize = "262144" // 256K
-	defaultDuration   = "20s"
-)
+var buffer []byte
 
 func main() {
 	flag.Parse()
+	buffer = make([]byte, *bufferSize)
 
 	http.HandleFunc("/", puke)
-	http.ListenAndServe(*addr, nil)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func puke(w http.ResponseWriter, r *http.Request) {
-	bufferSizeStr := r.FormValue("bufferSize")
-	if bufferSizeStr == "" {
-		bufferSizeStr = defaultBufferSize
-	}
-	bufferSize, err := strconv.Atoi(bufferSizeStr)
-	if err != nil {
-		http.Error(w, "invalid bufferSize param", 400)
-		return
-	}
+	var err error
+	var duration = *defaultDuration
 	durationStr := r.FormValue("duration")
-	if durationStr == "" {
-		durationStr = defaultDuration
+	if durationStr != "" {
+		duration, err = time.ParseDuration(durationStr)
+		if err != nil {
+			http.Error(w, "invalid duration param", 400)
+			return
+		}
 	}
-	duration, err := time.ParseDuration(durationStr)
-	if err != nil {
-		http.Error(w, "invalid duration param", 400)
-		return
-	}
-	var buffer = make([]byte, bufferSize)
 	var startTime = time.Now()
 	for {
 		_, err := w.Write(buffer)
 		if err != nil {
 			return
 		}
-		if time.Now().Sub(startTime) > duration {
+		if time.Since(startTime) > duration {
 			return
 		}
 	}
